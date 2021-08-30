b04fb728b94e04a5fbc94773f03f4fa16a87b8b7
方友松 <9569356+truexf@user.noreply.gitee.com>
2021-08-30 09:05:12 +0800

    + comment

--- consumer.go
+++ consumer.go
@@ -1,4 +1,11 @@
+// Copyright 2021 fangyousong(方友松). All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package kafkac
 
+// 由于kafka是持久化和基于消费position的可回溯消费，因此consumer端无需再过度封装。
+// 无论是低层api还是高层api，基于pattition或是组消费，kafka-go已经做得足够好。
+// 特此说明。
 type Consumer struct {
 }
